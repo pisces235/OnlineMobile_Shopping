053087d16a1c6431ecc62b42bd846d53feb4d6ce
pisces235 <dat200011003@gmail.com>
2021-07-16 14:19:59 +0700

    fix

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose')
  function connect() {
     try {
         mongoose.connect('mongodb+srv://pisces2305:Dat20011003@cluster0.113a0.mongodb.net/DA_Web?retryWrites=true&w=majority', {
+            useCreateIndex: true,
             useNewUrlParser: true,
-            useUnifiedTopology: true
+            useUnifiedTopology: true,
+            useFindAndModify: false
         })
         console.log('Connect sucessfuly!')
     } catch (error) {
