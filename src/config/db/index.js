819a66c480706646117ea78911ff3da2a1086c9d
pisces235 <dat200011003@gmail.com>
2021-07-16 15:19:08 +0700

    update

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -4,11 +4,14 @@ async function connect() {
     try {
         await mongoose.connect('mongodb+srv://pisces2305:Dat20011003@cluster0.113a0.mongodb.net/DA_Web?retryWrites=true&w=majority', {
             useNewUrlParser: true,
-            useUnifiedTopology: true
+            useUnifiedTopology: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
         })
         console.log('Connect sucessfuly!')
     } catch (error) {
-        console.log('Connect failure!')
+        console.log(error.message)
+        process.exit(1)
     }
 }
 
